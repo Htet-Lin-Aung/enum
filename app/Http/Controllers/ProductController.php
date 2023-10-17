4eeae32f262a346b1eee9ef4ea1960a64433bc9e
Htet Lin Aung <htetlinaungtuinfo@gmail.com>
2023-10-17 16:59:18 +0630

    added: features - products list,create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,21 +9,93 @@ use App\Enums\ProductStatusEnum;
 class ProductController extends Controller
 {
     /**
-     * Write code on Method
+     * Display a listing of the resource.
      *
-     * @return response()
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $input = [
-            'name' => 'Gold',
-            'body' => 'This is a Gold',
-            'status' => ProductStatusEnum::Active
-        ];
-  
-        $product = Product::create($input);
-  
-        dd($product->status, $product->status->value);
-  
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'body' => 'required',
+            'status' => 'required|in:pending,active,inactive,rejected'
+        ]);
+
+        $product = Product::create([
+            'name' => $request->name,
+            'body' => $request->body,
+            'status' => $request->status
+        ]);
+
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('product-test', [ProductController::class, 'index']);
+    Route::resource('products', ProductController::class);
 });
 
 require __DIR__.'/auth.php';
